5447fd49ceb861fe9ad5c02c7a7dd44d70e243e0
Jesse Cooper <jesse@iterate.co>
2018-02-11 23:24:06 -0800

    removed @Override annotations from BeaconBroadcastPackage #5

--- android/src/main/java/com/ibeacon/simulator/BeaconBroadcastPackage.java
+++ android/src/main/java/com/ibeacon/simulator/BeaconBroadcastPackage.java
@@ -12,17 +12,14 @@ import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 
 public class BeaconBroadcastPackage implements ReactPackage {
-    @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new BeaconBroadcast(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
     }
